86bdd0e038fd2aaa76b00d4e0b87984f0c044b56
unknown <joshuafrellt@gmail.com>
2025-12-17 23:12:46 +0800

    feat: alternate O and X, winner function

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,74 @@
-function Square() {
+"use client"
+import { useState } from 'react'
+import Clyde from './clyde'
+
+function Square({ value, onSquareClick }) {
   return (
-    <button className='w-9 h-9 font-bold text-2xl border-1 border-gray-500 cursor-pointer hover:bg-gray-100'>1</button>
+    <button 
+      className='w-9 h-9 m-0 text-xl font-bold border border-gray-500 cursor-pointer hover:bg-gray-100'
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
   )
 }
 
 export default function Board() {
-  <>
-    <div>
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div>
-      <Square />
-      <Square />
-      <Square />
-    </div>
-    <div>
-      <Square />
-      <Square />
-      <Square />
-    </div>
-  </>
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return
+    }
+    const nextSquares = squares.slice()
+    if (xIsNext) {
+      nextSquares[i] = 'X'
+    }
+    else {
+      nextSquares[i] = 'O'
+    }
+    setSquares(nextSquares)
+    setXIsNext(!xIsNext)
+  }
+
+  return (
+    <>
+      <div className='flex'>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className='flex'>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className='flex'>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
+    </>
+  )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i=0; i<lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
 }
